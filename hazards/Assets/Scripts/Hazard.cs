848c20b995dff965958407b76b55affd9819890c
megan alonzo <megan.alonzo@loop.colum.edu>
2018-11-28 19:41:22 -0600

    more scripting

--- hazards/Assets/Scripts/Hazard.cs
+++ hazards/Assets/Scripts/Hazard.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Hazard : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("something entered the hazard");
+        if (collision.gameObject.CompareTag("player"))
+        {
+            Debug.Log("player entered hazard");
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            Debug.Log("something other than the player entered the hazard");
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
